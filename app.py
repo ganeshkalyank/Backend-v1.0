7ba23511053f81bdceb7df95dffeac93912c241f
ganeshkalyank <srikgk333@gmail.com>
2022-12-03 03:46:25 +0530

    secured keys

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from flask_cors import CORS
 from dataclasses import dataclass
+import keys
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = keys.DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'kgkltd.tk@gmail.com'
-app.config['MAIL_PASSWORD'] = 'elzsxwurxjajxryn'
+app.config['MAIL_PASSWORD'] = keys.SMTP_PWD
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
@@ -61,7 +62,7 @@ def contact():
 
 @app.route("/contact_responses",methods=["GET"])
 def contact_responses():
-    if request.args.get("code") == "Kalyan8976":
+    if request.args.get("code") == keys.RESPONSES_PWD:
         responses = ContactResponses.query.all()
     else:
         return {
