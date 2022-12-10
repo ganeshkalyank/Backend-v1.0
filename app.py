dbcf548c2de04e639e31dee04bd7336aa86ebc74
ganeshkalyank <srikgk333@gmail.com>
2022-12-10 20:53:55 +0530

    added captcha support

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 from flask_cors import CORS
 from dataclasses import dataclass
+from requests import post
 import keys
 
 app = Flask(__name__)
@@ -33,6 +34,10 @@ class ContactResponses(db.Model):
     email = db.Column(db.String(200))
     message = db.Column(db.String(1000))
 
+@app.route("/")
+def index():
+    return jsonify({"msg":"Server is running!"})
+
 @app.route("/contact",methods=["GET","POST"])
 def contact():
     if request.method=="POST":
@@ -40,17 +45,27 @@ def contact():
         form_email = request.form.get("email")
         form_message = request.form.get("message")
         form_data = ContactResponses(name=form_name,email=form_email,message=form_message)
+        form_token = request.form.get("rctoken")
+        r = post("https://www.google.com/recaptcha/api/siteverify",data={"secret":keys.RECAPTCHA_TOKEN,"response":form_token,"remoteip":request.remote_addr})
         alert_email = Message(
             subject = "New form response from Portfolio",
             sender = ("Ganesh Kalyan Kommisetti", "kgkltd.tk@gmail.com"),
             recipients = ["srikgk333@gmail.com"],
             body = f"Name: {form_name}\nEmail: {form_email}\nMessage: {form_message}\n"
         )
-        mail.send(alert_email)
-        db.session.add(form_data)
-        db.session.commit()
-        status = "success"
-        message = "Message sent successfully!"
+        if r.json().get("success") and r.json().get("score")>0.5:
+            try:
+                mail.send(alert_email)
+                db.session.add(form_data)
+                db.session.commit()
+                status = "success"
+                message = "Message sent successfully!"
+            except:
+                status = "error"
+                message = "There was an internal error!"
+        else:
+            status = "error"
+            message = "reCAPTCHA verification failed!"
     else:
         status = "error"
         message = "Method not allowed!"
